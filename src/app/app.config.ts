a5fd564c0524ad9fcc55a557d5f41a71972556a8
Nadine Wirtgen <nadine.wirtgen@gmx.de>
2026-01-30 08:58:06 +0100

    Start Join with Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,21 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), 
+    provideFirebaseApp(() => initializeApp({ 
+      projectId: "join-70d12", 
+      appId: "1:663559193457:web:333562150e2375c669acd0", 
+      storageBucket: "join-70d12.firebasestorage.app", 
+      apiKey: "AIzaSyDtJzE_92zgvwjKa80_735rTBq5-IKvdso", 
+      authDomain: "join-70d12.firebaseapp.com", 
+      messagingSenderId: "663559193457"})), 
+    provideFirestore(() => getFirestore())
   ]
 };
