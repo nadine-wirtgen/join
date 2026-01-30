9889974496202eb0e6e3cbba9f9392a997f60b44
Nadine Wirtgen <nadine.wirtgen@gmx.de>
2026-01-30 09:03:46 +0100

    add: Authenticator

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), 
+    provideRouter(routes),  
+    provideFirestore(() => getFirestore()), 
     provideFirebaseApp(() => initializeApp({ 
       projectId: "join-70d12", 
       appId: "1:663559193457:web:333562150e2375c669acd0", 
@@ -17,6 +19,7 @@ export const appConfig: ApplicationConfig = {
       apiKey: "AIzaSyDtJzE_92zgvwjKa80_735rTBq5-IKvdso", 
       authDomain: "join-70d12.firebaseapp.com", 
       messagingSenderId: "663559193457"})), 
-    provideFirestore(() => getFirestore())
+      provideAuth(() => getAuth()), 
+      provideFirestore(() => getFirestore())
   ]
 };
