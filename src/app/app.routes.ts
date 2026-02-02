883b104bb668bade702db3e7554b412c8d2f3e9f
AltinTorba <alabto@gmail.com>
2026-02-02 20:21:56 +0100

    Add header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HelpSection } from './help-section/help-section';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'help-section', component: HelpSection}
+];
