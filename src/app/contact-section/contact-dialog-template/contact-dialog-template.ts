1881a91c6c18323413b102e3f83621bb3947791f
Wirtgen <nadine.wirtgen@gmx.de>
2026-02-02 12:06:54 +0100

    add: contact-dialog-template

---
+++ src/app/contact-section/contact-dialog-template/contact-dialog-template.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-dialog-template',
+  imports: [],
+  templateUrl: './contact-dialog-template.html',
+  styleUrl: './contact-dialog-template.scss',
+})
+export class ContactDialogTemplate {
+
+}
