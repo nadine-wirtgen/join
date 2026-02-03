4d5cf0e7bfd8aa597f0c001503e94d4458b86e7b
AltinTorba <alabto@gmail.com>
2026-02-03 21:25:41 +0100

    header help button activated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -10,24 +14,31 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),  
-    provideFirestore(() => getFirestore()), 
-    provideFirebaseApp(() => initializeApp({ 
-      projectId: "join-70d12", 
-      appId: "1:663559193457:web:333562150e2375c669acd0", 
-      storageBucket: "join-70d12.firebasestorage.app", 
-      apiKey: "AIzaSyDtJzE_92zgvwjKa80_735rTBq5-IKvdso", 
-      authDomain: "join-70d12.firebaseapp.com", 
-      messagingSenderId: "663559193457"})), 
-      provideAuth(() => getAuth()), 
-      provideFirestore(() => getFirestore()), 
-      provideFirebaseApp(() => initializeApp({ 
-        projectId: "join-70d12", 
-        appId: "1:663559193457:web:333562150e2375c669acd0", 
-        storageBucket: "join-70d12.firebasestorage.app", 
-        apiKey: "AIzaSyDtJzE_92zgvwjKa80_735rTBq5-IKvdso", 
-        authDomain: "join-70d12.firebaseapp.com", 
-        messagingSenderId: "663559193457" 
-      })), 
-  ]
+    provideRouter(routes),
+    provideFirestore(() => getFirestore()),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'join-70d12',
+        appId: '1:663559193457:web:333562150e2375c669acd0',
+        storageBucket: 'join-70d12.firebasestorage.app',
+        apiKey: 'AIzaSyDtJzE_92zgvwjKa80_735rTBq5-IKvdso',
+        authDomain: 'join-70d12.firebaseapp.com',
+        messagingSenderId: '663559193457',
+      }),
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'join-70d12',
+        appId: '1:663559193457:web:333562150e2375c669acd0',
+        storageBucket: 'join-70d12.firebasestorage.app',
+        apiKey: 'AIzaSyDtJzE_92zgvwjKa80_735rTBq5-IKvdso',
+        authDomain: 'join-70d12.firebaseapp.com',
+        messagingSenderId: '663559193457',
+      }),
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
