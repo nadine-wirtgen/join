18ac44d12a2f0d4933e456e49f495beccd3da1a0
Frank-Meck <frankmeckel@ymail.com>
2026-02-03 20:48:08 +0100

    leagel-notice, help-section, privacy-policy-section created

--- src/app/help-section/help-section.ts
+++ src/app/help-section/help-section.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { NgIf } from '@angular/common'; 
 @Component({
   selector: 'app-help-section',
-  imports: [],
+  standalone: true,
+  imports: [NgIf], 
   templateUrl: './help-section.html',
-  styleUrl: './help-section.scss',
+  styleUrls: ['./help-section.scss'],
 })
 export class HelpSection {
+  isVisible = true;
+  constructor(private router: Router) {}
 
+  closeHelp() {
+    this.router.navigate(['/summary']);
+  }
 }
