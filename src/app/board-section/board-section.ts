15ffe320c35c057b7067712b15c5492ae1fb8b3c
PatLiCoding <patricia.linne@yahoo.de>
2026-02-10 07:32:17 +0100

    add: search textarea

--- src/app/board-section/board-section.ts
+++ src/app/board-section/board-section.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-board-section',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './board-section.html',
   styleUrl: './board-section.scss',
 })
 export class BoardSection {
-
+  hoveredIcon: string | null = null;
+  searchValue = '';
 }
