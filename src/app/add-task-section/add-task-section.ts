a6fd7e21a6eaf85208ddd331e3f9cf77758752c5
Nadine Wirtgen <nadine.wirtgen@gmx.de>
2026-02-19 17:11:22 +0100

    fix: dialog/add-task -style

--- src/app/add-task-section/add-task-section.ts
+++ src/app/add-task-section/add-task-section.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AddTaskTemplate } from './add-task-template/add-task-template';
+import { AddTask } from './add-task/add-task';
 
 
 @Component({
   selector: 'app-add-task-section',
   standalone: true,
-  imports: [CommonModule, AddTaskTemplate],
+  imports: [CommonModule, AddTask],
   templateUrl: './add-task-section.html',
   styleUrls: ['./add-task-section.scss']
 })
