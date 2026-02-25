5105afbb69c9fe01ccd19cdf166f1c8713be1850
PatLiCoding <patricia.linne@yahoo.de>
2026-02-25 10:06:06 +0100

    add: environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'join-70d12',
+    appId: '1:663559193457:web:333562150e2375c669acd0',
+    storageBucket: 'join-70d12.firebasestorage.app',
+    apiKey: 'AIzaSyDtJzE_92zgvwjKa80_735rTBq5-IKvdso',
+    authDomain: 'join-70d12.firebaseapp.com',
+    messagingSenderId: '663559193457',
+  },
+};
