a07826149796daacac0e783d521b8c60c2cfadf8
nadine-wirtgen <nadine.wirtgen@gmx.de>
2026-03-02 09:14:38 +0100

    clean: add task section (comments with JD-doc standart)

--- src/app/add-task-section/add-task-dialog/add-task-dialog.ts
+++ src/app/add-task-section/add-task-dialog/add-task-dialog.ts
@@ -7,16 +7,18 @@ import { AddTaskTemplate } from '../add-task-template/add-task-template';
   standalone: true,
   imports: [CommonModule, AddTaskTemplate],
   templateUrl: './add-task-dialog.html',
-  styleUrls: ['./add-task-dialog.scss'], // styleUrl -> styleUrls
+  styleUrls: ['./add-task-dialog.scss'],
 })
 export class AddTaskDialog {
   @Output() close = new EventEmitter<void>();
 
-  // NEU: Input für Sichtbarkeit des Dialogs
   @Input() isDialogOpen: boolean = false;
 
   @Input() column: 'todo' | 'in-progress' | 'await-feedback' | 'done' = 'todo';
 
+  /**
+   * Emits the close event to notify parent component to close the dialog.
+   */
   onClose() {
     this.close.emit();
   }
